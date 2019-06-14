eddeb2bc38a1974fafc9bb2908cbc067c6853e40
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-14 12:29:32 -0600

    good to go

--- app/controllers/api/departments_controller.rb
+++ app/controllers/api/departments_controller.rb
@@ -1,24 +1,22 @@
-class Api::DepsController < ApplicationController
+class Api::DepartmentsController < ApplicationController
   before_action :set_dep, only: [:show, :update, :destroy]
 
   def index
     render json: Department.all
+
   end
 
   def show
     render json: @dep
   end
-
   def create
-    dep = Dep.new(dep_params)
-
+    dep = Department.new(dep_params)
     if dep.save
       render json: dep
     else
       render json: dep.errors, status: 422
     end
   end
-
   def update
     if @dep.update(dep_params)
       render json: @dep
@@ -26,17 +24,16 @@ class Api::DepsController < ApplicationController
       render json: @dep.errors, status: 422
     end
   end
-
+  
   def destroy
     @dep.destroy
   end
 
   private
-
   def set_dep
     @dep = Department.find(params[:id])
   end
-
   def dep_params
-    params.require(:dep).permit(:name)
+    params.require(:department).premit(:name)
   end
+end
