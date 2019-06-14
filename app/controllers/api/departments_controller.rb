23844aec2d51b6a7fb76c02784d6904f3fbd651a
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-14 11:03:01 -0600

    stubbed out item and department control

--- app/controllers/api/departments_controller.rb
+++ app/controllers/api/departments_controller.rb
@@ -1,10 +1,38 @@
 class Api::DepartmentsController < ApplicationController
+  before_action set_dep:, only: [:show, :udpate, :destroy]
   def index
+    render json: Department.all
+
   end
 
+  def show
+    render json: @dep
+  end
   def create
+    dep = Department.new(dep_params)
+    if dep.save
+      render json: dep
+    else
+      render json: dep.errors, status: 422
+    end
+  end
+  def update
+    if @dep.update(dep_params)
+      render json: @dep
+    else
+      render json: @dep.errors. status: 422
+    end
+  end
+  
+  def destroy
+    @dep.destroy
   end
 
-  def destroy
+  private
+  def set_dep
+    @dep = Department.find(params[:id])
+  end
+  def dep_params
+    params.require(:department).premit(:name)
   end
 end
