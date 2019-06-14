1a0cf67be7ec165c66ffbc2fc507c4521bfc42c0
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-14 11:10:28 -0600

    stubbed out reviews

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,2 +1,48 @@
 class Api::ReviewsController < ApplicationController
+  before_action set_item:
+  before_action set_rev:, only: [:show, :update, :destroy]
+
+  def index
+    render json: @item.reviews
+  end
+
+  def show
+    render json: @rev
+  end
+
+  def create
+    rev = @item.reviews.new(rev_params)
+    if rev.save
+      render json: rev
+    else
+      render json: rev.errors, status: 422
+    end
+  end
+
+  def update
+    if @rev.update(rev_params)
+      render json: @rev
+    else
+      render json: @rev.errors, status: 422
+    end
+  end
+
+  def destroy
+    @rev.destroy
+  end
+
+  private 
+  
+  def set_rev
+    @rev = Review.find(params[:id])
+  end
+
+  def rev_params
+    params.require(:item).permit(:title, :body, :author, :image_url)
+  end
+
+  def set_item
+    @item = Item.find(:item_id)
+  end
+
 end
