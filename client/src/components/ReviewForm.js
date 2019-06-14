e968dd98a88ec6b1af6b98f775e238eb73f58f6d
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-14 15:15:19 -0600

    set up

--- client/src/components/ReviewForm.js
+++ client/src/components/ReviewForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios'
-import { Form, Rating, Dropdown } from 'semantic-ui-react'
+import { Form, Rating, Dropdown, ItemImage } from 'semantic-ui-react'
 
 
 class ReviewForm extends React.Component{
@@ -21,16 +21,73 @@ class ReviewForm extends React.Component{
   }
 
   handleRating = (e, {rating}) =>{
-    this.setState({raiting})
+    this.setState({rating})
   }
 
   handleImage = (e)=>{
     const image_url = e.target.currentSrc
-    this.setState({image_url})
+    this.setState({ image_url })
   }
-  
-  render()
-  {
+  dropdownImageSelect = () =>{
+    const {image_url} = this.state
+    const images = [
+      {
+        text: 'blue',
+        value: 'https://robohash.org/sitsequiquia.png?size=300x300&set=set1',
+        image:{
+          avatar: true,
+          src: "https://robohash.org/sitsequiquia.png?size=300x300&set=set1"
+        },
+      },
+      {
+        text: 'purple',
+        value: 'https://robohash.org/sitsequiquia.png?size=300x300&set=set2',
+        image:{
+          avatar: true,
+          src: "https://robohash.org/sitsequiquia.png?size=300x300&set=set2"
+        },
+      },
+      {
+        text: 'green',
+        value: 'https://robohash.org/sitsequiquia.png?size=300x300&set=set3',
+        image:{
+          avatar: true,
+          src: "https://robohash.org/sitsequiquia.png?size=300x300&set=set3"
+        },
+      },
+    ]
+    return(
+      <Dropdown
+      name='image'
+      placeholder="Select an Avatar"
+      compact
+      options={images}
+      value = {image_url}
+      onChange={this.handleImage}
+      />
+    )
+  }
+
+  handleSubmit = (e) =>{
+    e.preventDefault()
+    if (this.props.edit){
+      const {match: {params: {id}}}= this.props
+      const {location: {state:{item_id} }} = this.props
+      axios.put(`/api/items/${item_id}/reviews/${id}`, {...this.state})
+        .then(res =>{
+          this.props.history.goBack()
+        })
+
+    } else{
+      axios.post(`/api/items/${item_id}/reviews`, {...this.state})
+        .then(res=> this.props.addReview(res.data))
+        this.props.toggle()
+        
+    }
+  }
+
+  render(){
+
     return(
       <>
         <Form>
